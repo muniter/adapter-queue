9a659ce9e50a1bf364918d8cba54ea6f272471cd
Javier Lopez <graulopezjavier@gmail.com>
2025-06-21 11:13:59 -0500

    remove dist

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-export { Queue } from './core/queue.ts';
-export { DbQueue } from './drivers/db.ts';
-export { SqsQueue } from './drivers/sqs.ts';
-export { Worker, runWorker } from './worker/worker.ts';
-export { Job, RetryableJob, JobStatus, JobMeta, QueueMessage, QueueEvent } from './interfaces/job.ts';
-export { DatabaseAdapter, QueueJobRecord } from './interfaces/database.ts';
-export { Serializer, JsonSerializer, DefaultSerializer } from './core/serializer.ts';
-export type { SQSClient } from './drivers/sqs.ts';
-export type { WorkerOptions } from './worker/worker.ts';
